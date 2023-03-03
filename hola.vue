8ab6097fba2140545c7b533067618630abf5e348
Robert Medina <csoperativo@gmail.com>
2023-03-03 04:36:58 -0400

    introduccion a vue

---
+++ hola.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<div>{{text}}</div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "hola"
+        }
+    },
+}
+</script>
+
+<style>
+    div{
+        color: red;
+    }
+</style>
